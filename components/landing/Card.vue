ecda0b8d3f918226bd3a559fce18dbd802657ea5
S4ji <yelhafny@antidots-group.com>
2024-12-13 12:33:20 +0100

    fix size-of-cards

--- components/landing/Card.vue
+++ components/landing/Card.vue
@@ -7,32 +7,33 @@ defineProps(['plan'])
         <div
             class="flex flex-col w-full order-first lg:order-none border-2 border-[#489d9d25] border-opacity-50 py-5 px-6 rounded-md h-[700px]"
         >
-            <div class="flex flex-col items-center justify-between flex-grow">
-                <h4 class="text-lg font-medium text-teal-100 mb-4 text-center">
+            <div class="flex justify-center mb-4">
+                <h4 class="text-lg font-medium text-teal-100 text-center">
                     {{ plan.name }}
                 </h4>
-
-                <div class="flex justify-center mb-4">
-                    <img
-                        class="rounded-full w-48 h-48 object-contain transition-transform transform hover:scale-125"
-                        :src="plan.link"
-                        alt="Starship starts the engine"
-                        loading="eager"
-                        format="avif"
-                    />
-                </div>
-
-                <p class="mt-3 text-4xl font-bold text-teal-50 md:text-4xl">
-                    {{
-                        plan.price && typeof plan.price === 'object'
-                            ? plan.price.monthly
-                            : plan.price
-                    }}
-                </p>
             </div>
 
-            <!-- Features List -->
-            <ul class="mt-8 space-y-4 flex-grow overflow-y-auto">
+            <div class="flex justify-center mb-4">
+                <img
+                    class="rounded-full w-48 h-48 object-contain transition-transform transform hover:scale-125"
+                    :src="plan.link"
+                    alt="Image description"
+                    loading="eager"
+                    format="avif"
+                />
+            </div>
+
+            <p
+                class="mt-3 text-4xl font-bold text-teal-50 md:text-4xl text-center"
+            >
+                {{
+                    plan.price && typeof plan.price === 'object'
+                        ? plan.price.monthly
+                        : plan.price
+                }}
+            </p>
+
+            <ul class="mt-6 space-y-4 overflow-y-auto">
                 <li
                     v-for="item of plan.features"
                     class="flex items-start gap-3 text-teal-100"
