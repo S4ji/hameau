e8183321f692920408a6f5fe8f1d2130b6356520
S4ji <yelhafny@antidots-group.com>
2025-01-07 16:25:38 +0100

    fix:lore parts menu

--- components/landing/Navbar.vue
+++ components/landing/Navbar.vue
@@ -5,20 +5,28 @@ const menuitems = [
         path: '/',
     },
     {
-        title: 'Races',
-        path: '/races',
-    },
-    {
-        title: 'Métiers',
-        path: '/metier',
-    },
-    {
-        title: 'Magie',
-        path: '/magie',
-    },
-    {
         title: 'Règlement',
         path: '/reglement',
+    },
+    {
+        title: 'Lore du serveur',
+        path: '/background',
+        icon: 'entypo:arrow-down', // Icon for lore
+
+        children: [
+            {
+                title: 'Races',
+                path: '/races',
+            },
+            {
+                title: 'Métiers',
+                path: '/metier',
+            },
+            {
+                title: 'Magie',
+                path: '/magie',
+            },
+        ],
     },
     {
         title: 'Notre équipe',
@@ -41,9 +49,9 @@ const open = ref(false)
             class="flex flex-col lg:flex-row justify-between items-center my-5"
         >
             <div class="flex w-full lg:w-auto items-center justify-between">
-                <a href="/" class="text-lg"
-                    ><img :src="logo" alt="" width="100" height="120"
-                /></a>
+                <a href="/" class="text-lg">
+                    <img :src="logo" alt="" width="100" height="120" />
+                </a>
                 <div class="block lg:hidden">
                     <button @click="open = !open" class="text-gray-800">
                         <svg
@@ -73,13 +81,41 @@ const open = ref(false)
                 :class="{ block: open, hidden: !open }"
             >
                 <ul class="flex flex-col lg:flex-row lg:gap-3">
-                    <li v-for="item of menuitems" v-bind:key="item.title">
+                    <li
+                        v-for="item of menuitems"
+                        :key="item.title"
+                        class="relative group"
+                    >
                         <a
                             :href="item.path"
-                            class="flex lg:px-3 py-2 text-teal-50 hover:text-cyan-100"
+                            class="flex items-center justify-center lg:px-3 py-2 text-teal-50 hover:text-cyan-100"
                         >
-                            {{ item.title }}
+                            <span class="flex items-center">
+                                {{ item.title }}
+                                <Icon
+                                    class="ml-2 text-white"
+                                    :name="item.icon"
+                                />
+                            </span>
                         </a>
+                        <!-- Submenu centered -->
+                        <ul
+                            v-if="item.children"
+                            class="absolute right-4 transform -translate-x-1/2 top-full hidden group-hover:block"
+                        >
+                            <li
+                                v-for="subItem of item.children"
+                                :key="subItem.title"
+                                class="bg-white text-teal-950"
+                            >
+                                <a
+                                    :href="subItem.path"
+                                    class="block text-teal-950 hover:text-teal-50 hover:bg-teal-950 p-2"
+                                >
+                                    {{ subItem.title }}
+                                </a>
+                            </li>
+                        </ul>
                     </li>
                 </ul>
             </nav>
---
+++ pages/background.vue
@@ -0,0 +1,77 @@
+<script setup>
+definePageMeta({
+    layout: 'landing',
+})
+</script>
+
+<template>
+    <LandingContainer>
+        <LandingSectionhead>
+            <template v-slot:title>Lore du serveur </template>
+            <template v-slot:desc>
+                <p class="pb-4">
+                    C'est dans ce décor sombre et énigmatique que se déroule le
+                    destin des nouveaux venus, cherchant à s'adapter, à percer
+                    les secrets des terres sauvages et, peut-être, à retrouver
+                    un jour leur foyer.
+                </p>
+                <p class="text-teal-100 text-justify pb-4">
+                    Dans les profondeurs des terres sauvages, là où les horizons
+                    s'étirent à perte de vue et où la brume épaisse enveloppe
+                    tout comme un voile mystérieux, une force ancienne et
+                    insondable réside. C'est un royaume où la nature est à la
+                    fois majestueuse et cruelle, où les échos du passé résonnent
+                    dans chaque souffle de vent et chaque frémissement des
+                    feuilles.
+                </p>
+
+                <p class="text-teal-100 text-justify pb-4">
+                    Des légendes parlent de ce lieu comme d'une terre autrefois
+                    prospère, où les hommes et les bêtes coexistaient en
+                    harmonie, mais quelque chose a changé. Un mal indicible
+                    s'est éveillé, se répandant lentement mais inexorablement à
+                    travers les terres sauvages, corrompant tout sur son
+                    passage. Des créatures autrefois paisibles sont devenues des
+                    prédateurs féroces, tandis que des abominations monstrueuses
+                    errent dans l'obscurité.
+                </p>
+
+                <p class="text-teal-100 text-justify pb-4">
+                    Les nouveaux arrivants, arrachés à leur monde par un portail
+                    mystérieux leurs prenant tous leurs pouvoirs et précipités
+                    dans cet univers inhospitalier, se retrouvent désormais
+                    confrontés à un destin incertain. Le portail, une fois
+                    franchi, se referme irrémédiablement, condamnant les
+                    voyageurs à demeurer dans les terres sauvages sans espoir de
+                    retour.
+                </p>
+
+                <p class="text-teal-100 text-justify pb-4">
+                    Par chance, un hameau isolé se dresse au cœur de ce chaos,
+                    offrant un refuge temporaire pour ceux qui osent s'aventurer
+                    dans ces contrées dangereuses. Dernière barrière fragile
+                    contre les menaces qui rôdent au-dehors. Mais même là, la
+                    sécurité est précaire, car les forces du mal ne sont jamais
+                    loin, guettant le moment propice pour frapper.
+                </p>
+
+                <p class="text-teal-100 text-justify pb-4">
+                    Les habitants du hameau vivent dans la crainte constante,
+                    sachant que chaque jour pourrait être leur dernier.
+                    Pourtant, ils se battent pour survivre, s'accrochant à
+                    l'espoir que peut-être, un jour, ils trouveront un moyen de
+                    lever la malédiction qui les retient captifs dans les terres
+                    sauvages.
+                </p>
+
+                <p class="text-teal-100 text-justify pb-4">
+                    C'est dans ce décor sombre et mystérieux que se joue le
+                    destin des nouveaux arrivants, alors qu'ils luttent pour
+                    s'adapter à leur nouvel environnement, découvrir les secrets
+                    cachés des terres sauvages et peut-être, un jour, trouver un
+                    moyen de rentrer chez eux.
+                </p>
+            </template>
+        </LandingSectionhead>
+    </LandingContainer>
+</template>
